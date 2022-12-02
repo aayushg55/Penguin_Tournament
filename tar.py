b2c585727e4fd66c194e986c62184cd96c69a09a
Aayush Gupta <aayushnj@gmail.com>
2022-12-02 08:56:22 -0800

    updated outputs

--- tar.py
+++
@@ -1,8 +0,0 @@
-from starter import *
-from anneal import *
-import math
-import multiprocess as mp
-import copy, random
-from collections import defaultdict, Counter
-import itertools
-tar('outputs', overwrite = True)
