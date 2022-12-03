68c4d3baaa43733464fcaa3c09142a4e591bfedc
Aayush Gupta <aayushnj@gmail.com>
2022-12-02 23:41:21 -0800

    Clean up

---
+++ tar.py
@@ -0,0 +1,10 @@
+from starter import *
+from anneal import *
+import math
+import multiprocess as mp
+import copy, random
+from collections import defaultdict, Counter
+import itertools
+from bs4 import BeautifulSoup
+import requests
+tar('outputs', overwrite = True)
